ddc83eba4b52ae0b07cde8876b842e46d2e33f71
recol <yoshioka@recol.co.jp>
2021-05-12 13:00:06 +0900

    after section6 results

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
+]
+
+module.exports = {
+  entry: './app/assets/scripts/App.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname,'app')
+  },
+  devServer: {
+    before: function(app,server){
+      server._watch('./app/**/*.html')
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  mode: 'development',
+  // watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader','css-loader?url=false',{loader:'postcss-loader',options:{postcssOptions:{plugins: postCSSPlugins}}}]
+      }
+    ]
+  }
+}
